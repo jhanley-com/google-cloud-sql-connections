ed3a4c4f1dced6667389a40fff2bc5711189ae0a
John J. Hanley <50686230+jhanley-com@users.noreply.github.com>
2019-08-15 23:20:20 -0700

    Add files via upload

---
+++ start_proxy_tcp.sh
@@ -0,0 +1,1 @@
+./cloud_sql_proxy --instances=myproject:us-central1:myinstance=tcp:3306,myproject:us-central1:myinstance-failover=tcp:3307,myproject:us-central1:myinstance-replica=tcp:3308
---
+++ start_proxy_unix.sh
@@ -0,0 +1,1 @@
+./cloud_sql_proxy -dir=/cloudsql --instances=myproject:us-central1:myinstance,myproject:us-central1:myinstance-failover,myproject:us-central1:myinstance-replica
